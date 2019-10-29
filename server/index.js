199600a627fd9c169fa59fe4a92bec287217811d
Christian Dibala Bell <ChristianDibalaBell@gmail.com>
2019-10-29 00:31:08 -0400

    Fetches repos

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+const helpers = require('../helpers/github.js');
+
 let app = express();
 
 
@@ -11,6 +13,14 @@ app.use(bodyParser());
 
 app.post('/repos', function (req, res) {
   console.log(`req.body: `, req.body);
+  const term = req.body.term;
+
+  // ({ term } = { req.body })
+
+  helpers.getReposByUsername(term, (err, data) => {
+    if (err) { return console.log(err) }
+    console.log(data);
+  });
 
   // TODO - your code here!
   // This route should take the github username provided
