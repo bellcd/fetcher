c08654ef1ec08ba49161204e82659d4a1f8186b5
Christian Dibala Bell <ChristianDibalaBell@gmail.com>
2019-11-13 21:46:27 -0500

    Adds basic card styling

--- client/src/repo.jsx
+++ client/src/repo.jsx
@@ -4,8 +4,8 @@ const Repo = ({ repo }) => {
   return (
     <>
       <div className="repo">
-      <span><a href={repo.html_url}>{repo.name}</a></span>
-      <span>{repo.updated_at}</span>
+      <div className="card-title"><a href={repo.html_url}>{repo.name}</a></div>
+      <div>Last Update At: {repo.updated_at}</div>
       </div>
     </>
   );
